cf61921f886afc13e5ed9223fb9fb58272705de7
JacobRPrice <JacobRPricePhD@gmail.com>
2022-11-02 13:40:41 -0400

    anova_script.sh: update file paths to reflect change in directory names (/r/ -> /src/).

--- src/anova_script.sh
+++ src/anova_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-Rscript anova_script.R AOA
-Rscript anova_script.R AOB
-Rscript anova_script.R nosZ
-Rscript anova_script.R netMIN
-Rscript anova_script.R netNIT
-Rscript anova_script.R NH4Nstock
-Rscript anova_script.R NO3Nstock
-Rscript anova_script.R OM_percent
-Rscript anova_script.R Moisture_percent
-Rscript anova_script.R GLU_gSoil
-Rscript anova_script.R NAG_gSoil
-Rscript anova_script.R PHO_gSoil
-Rscript anova_script.R GLU_gOM
-Rscript anova_script.R NAG_gOM
-Rscript anova_script.R PHO_gOM
+Rscript ./src/anova_script.R AOA
+Rscript ./src/anova_script.R AOB
+Rscript ./src/anova_script.R nosZ
+Rscript ./src/anova_script.R netMIN
+Rscript ./src/anova_script.R netNIT
+Rscript ./src/anova_script.R NH4Nstock
+Rscript ./src/anova_script.R NO3Nstock
+Rscript ./src/anova_script.R OM_percent
+Rscript ./src/anova_script.R Moisture_percent
+Rscript ./src/anova_script.R GLU_gSoil
+Rscript ./src/anova_script.R NAG_gSoil
+Rscript ./src/anova_script.R PHO_gSoil
+Rscript ./src/anova_script.R GLU_gOM
+Rscript ./src/anova_script.R NAG_gOM
+Rscript ./src/anova_script.R PHO_gOM
