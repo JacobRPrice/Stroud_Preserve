1bb9f793f226a76cc2423fee757c07313684ebd6
JacobRPrice <JacobRPricePhD@gmail.com>
2022-11-02 12:51:18 -0400

    anova_script.R: start.

---
+++ r/anova_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Rscript anova_script.R AOA
+Rscript anova_script.R AOB
+Rscript anova_script.R nosZ
+Rscript anova_script.R netMIN
+Rscript anova_script.R netNIT
+Rscript anova_script.R NH4Nstock
+Rscript anova_script.R NO3Nstock
+Rscript anova_script.R OM_percent
+Rscript anova_script.R Moisture_percent
+Rscript anova_script.R GLU_gSoil
+Rscript anova_script.R NAG_gSoil
+Rscript anova_script.R PHO_gSoil
+Rscript anova_script.R GLU_gOM
+Rscript anova_script.R NAG_gOM
+Rscript anova_script.R PHO_gOM
