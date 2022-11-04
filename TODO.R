2aaa4f46bb2f1b8f38e57ff55d0d366631305918
JacobRPrice <JacobRPricePhD@gmail.com>
2022-11-04 14:32:04 -0400

    TODO.R: start to do list.

---
+++ TODO.R
@@ -0,0 +1,27 @@
+
+# overall -----------------------------------------------------------------
+# TODO:
+# * Change variable names to something more descriptive/easier to read. 
+# * subset data to just the parameters we are reporting on. 
+
+
+# Main Text Figures: Boxplots ---------------------------------------------
+# will be based off of the process in anova_script.R
+# TODO: 
+#   * colors: 
+#       set conventional to black 
+#       differentiate between conv.cc.X and org.cc.X using colors 
+#   * place p-values in the figure (if it looks nice)
+#   * add black line at y=0 if y=0 exists on the y axis as normally plotted. 
+#   * make axis titles nice
+
+
+
+
+
+# Main Text Figures: Seasonal ---------------------------------------------
+# will be based off of the process in temporal_script.R
+# TODO: 
+#   * remove "value" from the y axis label
+#   * add black line at y=0 if y=0 exists on the y axis as normally plotted. 
+#   * colors: use colors to differentiate between years 
