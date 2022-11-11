d8d03fa8cf291d7db6a0e33dccf283296bb6ae23
JacobRPrice <JacobRPricePhD@gmail.com>
2022-11-11 11:19:49 -0500

    update TODO

--- TODO.R
+++ TODO.R
@@ -1,33 +1,15 @@
 
 # overall -----------------------------------------------------------------
-# TODO:
-# * Change variable names to something more descriptive/easier to read. 
-      # netMIN to Net Mineralization
-      # netNIT to Net Nitrification
-      # NH4Nstock to Soil Ammonium-N
-      # NO3Nstock to Soil Nitrate-N
-      # GLU_gSoil to GLU, and similar for other EEA
-# * subset data to just the parameters we are reporting on. 
 
-
-# Main Text Figures: Boxplots ---------------------------------------------
-# will be based off of the process in anova_script.R
+# ANOVA and boxplots ------------------------------------------------------
+# NOTE: 
+#     will be based off of the process in anova_script.R
 # TODO: 
-#   * colors: 
+#     * implement automated checking of each tests assumptions 
+#   * boxplot colors: 
 #       set conventional to black 
 #       differentiate between conv.cc.X and org.cc.X using colors 
 #   * place p-values in the figure (if it looks nice)
 #   * add black line at y=0 if y=0 exists on the y axis as normally plotted. 
 #   * make axis titles nice
 
-
-
-
-
-# Main Text Figures: Seasonal ---------------------------------------------
-# will be based off of the process in temporal_script.R
-# TODO: 
-#   * reorder the facets in the seasonal plots. 
-#   * remove "value" from the y axis label
-#   * add black line at y=0 if y=0 exists on the y axis as normally plotted. 
-#   * colors: use colors to differentiate between years 
