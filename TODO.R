cbb3d0545a54266f32544877de0dc55200fe1ee4
JacobRPrice <JacobRPricePhD@gmail.com>
2022-11-14 12:23:37 -0500

    update TODO.

--- TODO.R
+++ TODO.R
@@ -1,11 +1,10 @@
 
 # overall -----------------------------------------------------------------
 
-# ANOVA and boxplots ------------------------------------------------------
+# boxplots ----------------------------------------------------------------
 # NOTE: 
 #     will be based off of the process in anova_script.R
 # TODO: 
-#     * implement automated checking of each tests assumptions 
 #   * boxplot colors: 
 #       set conventional to black 
 #       differentiate between conv.cc.X and org.cc.X using colors 
@@ -13,3 +12,9 @@
 #   * add black line at y=0 if y=0 exists on the y axis as normally plotted. 
 #   * make axis titles nice
 
+
+# ANOVA -------------------------------------------------------------------
+# NOTE: 
+#     will be based off of the process in anova_script.R
+# TODO: 
+#     * implement automated checking of each tests assumptions 
