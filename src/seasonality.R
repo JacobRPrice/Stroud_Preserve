2e4d0624086f5fef8c2339db4fc52d0c7549e8ce
JacobRPrice <JacobRPricePhD@gmail.com>
2022-12-29 23:24:41 -0500

    Clean up (simplify) figures...
    seasonality.R:
      * improve x axis label
      * generate a single supplemental figure based off the main text figure.
      * comment out unneeded sub-figures for supplemental (these lines can be deleted later)
    
    boxplots.R:
      * comment out unneeded sub figures for supplemental (these lines can be deleted later)

--- src/seasonality.R
+++ src/seasonality.R
@@ -9,31 +9,31 @@ dat <- readRDS(file.path(getwd(), "/data/", "dat.RDS"))
 
 # subset by analysis type
 metadata <- c("Date", "Site", "Management_System" , "Tillage", "Cover_Crop", "Treatment_Group", "Year", "Month", "Day")
-qpcr <- subset(
-  dat, 
-  select = c(
-    metadata, 
-    c("AOA", "AOB", "nosZ")
-  )
-)
-NitMin <- subset(
-  dat,
-  select = c(
-    metadata, 
-    c("Net_Nitrification", "Soil_NH4N", "Net_Mineralization", "Soil_NO3N")
-  )
-)
-eea <- subset(
-  dat,
-  select = c(
-    metadata, 
-    c(
-      # "BG", "NAG", "AP",
-      "ln(BG)", "ln(NAG)", "ln(AP)", 
-      "NAG:BG", "NAG:AP"
-    )
-  )
-)
+# qpcr <- subset(
+#   dat, 
+#   select = c(
+#     metadata, 
+#     c("AOA", "AOB", "nosZ")
+#   )
+# )
+# NitMin <- subset(
+#   dat,
+#   select = c(
+#     metadata, 
+#     c("Net_Nitrification", "Soil_NH4N", "Net_Mineralization", "Soil_NO3N")
+#   )
+# )
+# eea <- subset(
+#   dat,
+#   select = c(
+#     metadata, 
+#     c(
+#       # "BG", "NAG", "AP",
+#       "ln(BG)", "ln(NAG)", "ln(AP)", 
+#       "NAG:BG", "NAG:AP"
+#     )
+#   )
+# )
 datmain <- subset(
   dat,
   select = c(
@@ -52,22 +52,22 @@ datmain <- subset(
 )
 
 # restructure data
-names(qpcr)
-qpcr <- pivot_longer(
-  data = qpcr, 
-  cols = names(qpcr)[-c(1:9)], 
-  names_to = "Parameter"
-)
-NitMin <- pivot_longer(
-  data = NitMin, 
-  cols = names(NitMin)[-c(1:9)], 
-  names_to = "Parameter"
-)
-eea <- pivot_longer(
-  data = eea, 
-  cols = names(eea)[-c(1:9)], 
-  names_to = "Parameter"
-)
+# names(qpcr)
+# qpcr <- pivot_longer(
+#   data = qpcr, 
+#   cols = names(qpcr)[-c(1:9)], 
+#   names_to = "Parameter"
+# )
+# NitMin <- pivot_longer(
+#   data = NitMin, 
+#   cols = names(NitMin)[-c(1:9)], 
+#   names_to = "Parameter"
+# )
+# eea <- pivot_longer(
+#   data = eea, 
+#   cols = names(eea)[-c(1:9)], 
+#   names_to = "Parameter"
+# )
 datmain <- pivot_longer(
   data = datmain, 
   cols = names(datmain)[-c(1:9)], 
@@ -76,22 +76,22 @@ datmain <- pivot_longer(
 
 
 # order parameters according to how they need to be displayed
-qpcr$Parameter <- factor(
-  qpcr$Parameter, 
-  levels = c("AOA", "AOB", "nosZ")
-)
-NitMin$Parameter <- factor(
-  NitMin$Parameter, 
-  levels = c("Net_Nitrification", "Soil_NH4N", "Net_Mineralization", "Soil_NO3N")
-)
-eea$Parameter <- factor(
-  eea$Parameter, 
-  levels = c(
-    # "BG", "NAG", "AP",
-    "ln(BG)", "ln(NAG)", "ln(AP)", 
-    "NAG:BG", "NAG:AP"
-  )
-)
+# qpcr$Parameter <- factor(
+#   qpcr$Parameter, 
+#   levels = c("AOA", "AOB", "nosZ")
+# )
+# NitMin$Parameter <- factor(
+#   NitMin$Parameter, 
+#   levels = c("Net_Nitrification", "Soil_NH4N", "Net_Mineralization", "Soil_NO3N")
+# )
+# eea$Parameter <- factor(
+#   eea$Parameter, 
+#   levels = c(
+#     # "BG", "NAG", "AP",
+#     "ln(BG)", "ln(NAG)", "ln(AP)", 
+#     "NAG:BG", "NAG:AP"
+#   )
+# )
 datmain$Parameter <- factor(
   datmain$Parameter,
   levels = c(
@@ -107,19 +107,96 @@ datmain$Parameter <- factor(
 )
 
 # remove NA entries
-qpcr <- qpcr %>% drop_na(value)
-NitMin <- NitMin %>% drop_na(value) 
-eea <- eea %>% drop_na(value) 
+# qpcr <- qpcr %>% drop_na(value)
+# NitMin <- NitMin %>% drop_na(value) 
+# eea <- eea %>% drop_na(value) 
 datmain <- datmain %>% drop_na(value)
 
 # supplemental plots ------------------------------------------------------
 # these plots 
-#   * based on the main plots above 
+#   * based on the main plots 
 #   * use color to distinguish between different treatment groups
 #   * use linetype to distinguish between years
 
-(apqpcr <- ggplot(
-  data = qpcr, 
+# (apqpcr <- ggplot(
+#   data = qpcr, 
+#   aes(
+#     x = Day,
+#     y = value, 
+#     color = Treatment_Group, 
+#     linetype = Year
+#   )
+# ) +
+#   theme_bw() +
+#   geom_point() +
+#   geom_smooth(se = FALSE, size = 0.5) +
+#   facet_grid(Parameter~., scales = "free_y") +
+#   scale_x_continuous(limits = c(60,340)) + 
+#   scale_y_continuous(trans = "log10") + 
+#   # ylab("Gene Copies / g Soil (Log10 Scaled)") +
+#   theme(
+#     axis.title.y = element_blank(), 
+#     panel.grid.major.x = element_blank(),
+#     legend.position = "bottom"
+#   ) +
+#   guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
+#   guides(linetype = guide_legend(nrow = 2)) +
+#   xlab("Day of the Year")
+# )
+# 
+# (apnitmin <- ggplot(
+#   data = NitMin, 
+#   aes(
+#     x = Day,
+#     y = value, 
+#     color = Treatment_Group, 
+#     linetype = Year
+#   )
+# ) +
+#     theme_bw() +
+#     geom_hline(yintercept = 0) +
+#     geom_point() +
+#     geom_smooth(se = FALSE, size = 0.5) +
+#     facet_grid(Parameter~., scales = "free_y") +
+#     # facet_wrap(Parameter ~., scales = "free_y", ncol = 2, dir = "v") +
+#     scale_x_continuous(limits = c(60,340)) + 
+#     theme(
+#       axis.title.y = element_blank(), 
+#       panel.grid.major.x = element_blank(),
+#       legend.position = "bottom"
+#     ) +
+#     guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
+#     guides(linetype = guide_legend(nrow = 2)) +
+#     xlab("Day of the Year")
+# )
+# 
+# (apeea <- ggplot(
+#   data = eea, 
+#   aes(
+#     x = Day,
+#     y = value, 
+#     color = Treatment_Group, 
+#     linetype = Year
+#   )
+# ) +
+#     theme_bw() +
+#     geom_point() +
+#     geom_smooth(se = FALSE, size = 0.5) +
+#     facet_grid(Parameter~., scales = "free_y") +
+#     # facet_wrap(Parameter~., scales = "free_y", ncol = 2, dir = "v") +
+#     scale_x_continuous(limits = c(60,340)) + 
+#     theme(
+#       axis.title.y = element_blank(), 
+#       panel.grid.major.x = element_blank(),
+#       legend.position = "bottom"
+#     ) +
+#     guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
+#     guides(linetype = guide_legend(nrow = 2)) +
+#     xlab("Day of the Year")
+# )
+
+(apall <- ggplot(
+  data = datmain, 
   aes(
     x = Day,
     y = value, 
@@ -130,88 +207,42 @@ datmain <- datmain %>% drop_na(value)
   theme_bw() +
   geom_point() +
   geom_smooth(se = FALSE, size = 0.5) +
-  facet_grid(Parameter~., scales = "free_y") +
+  facet_wrap(Parameter~., scales = "free_y", ncol = 2, dir = "v") +
   scale_x_continuous(limits = c(60,340)) + 
-  scale_y_continuous(trans = "log10") + 
-  # ylab("Gene Copies / g Soil (Log10 Scaled)") +
   theme(
     axis.title.y = element_blank(), 
     panel.grid.major.x = element_blank(),
     legend.position = "bottom"
   ) +
   guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
-  guides(linetype = guide_legend(nrow = 2))
+    guides(linetype = guide_legend(nrow = 2)) +
+    xlab("Day of the Year")
 )
-
-(apnitmin <- ggplot(
-  data = NitMin, 
-  aes(
-    x = Day,
-    y = value, 
-    color = Treatment_Group, 
-    linetype = Year
-  )
-) +
-    theme_bw() +
-    geom_hline(yintercept = 0) +
-    geom_point() +
-    geom_smooth(se = FALSE, size = 0.5) +
-    facet_grid(Parameter~., scales = "free_y") +
-    # facet_wrap(Parameter ~., scales = "free_y", ncol = 2, dir = "v") +
-    scale_x_continuous(limits = c(60,340)) + 
-    theme(
-      axis.title.y = element_blank(), 
-      panel.grid.major.x = element_blank(),
-      legend.position = "bottom"
-    ) +
-    guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
-    guides(linetype = guide_legend(nrow = 2))
-)
-
-(apeea <- ggplot(
-  data = eea, 
-  aes(
-    x = Day,
-    y = value, 
-    color = Treatment_Group, 
-    linetype = Year
-  )
-) +
-    theme_bw() +
-    geom_point() +
-    geom_smooth(se = FALSE, size = 0.5) +
-    facet_grid(Parameter~., scales = "free_y") +
-    # facet_wrap(Parameter~., scales = "free_y", ncol = 2, dir = "v") +
-    scale_x_continuous(limits = c(60,340)) + 
-    theme(
-      axis.title.y = element_blank(), 
-      panel.grid.major.x = element_blank(),
-      legend.position = "bottom"
-    ) +
-    guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
-    guides(linetype = guide_legend(nrow = 2))
-)
-
 
 # save figures ------------------------------------------------------------
+# ggsave(
+#   plot = apqpcr,
+#   filename = file.path(getwd(), "figs", "2020_vs_2021_qPCR.pdf"), 
+#   # width = 6, height = 3*2, units = "in"
+#   width = 6, height = 6, units = "in"
+# )
+# ggsave(
+#   plot = apnitmin,
+#   filename = file.path(getwd(), "figs", "2020_vs_2021_NitMin.pdf"), 
+#   # width = 6, height = 4*2, units = "in"
+#   width = 6, height = 6, units = "in"
+# )
+# ggsave(
+#   plot = apeea,
+#   filename = file.path(getwd(), "figs", "2020_vs_2021_EEA.pdf"), 
+#   # width = 6, height = 5*2, units = "in"
+#   # width = 6, height = 18*2, units = "in"
+#   width = 6, height = 8, units = "in"
+# )
 ggsave(
-  plot = apqpcr,
-  filename = file.path(getwd(), "figs", "2020_vs_2021_qPCR.pdf"), 
-  # width = 6, height = 3*2, units = "in"
-  width = 6, height = 6, units = "in"
-)
-ggsave(
-  plot = apnitmin,
-  filename = file.path(getwd(), "figs", "2020_vs_2021_NitMin.pdf"), 
-  # width = 6, height = 4*2, units = "in"
-  width = 6, height = 6, units = "in"
-)
-ggsave(
-  plot = apeea,
-  filename = file.path(getwd(), "figs", "2020_vs_2021_EEA.pdf"), 
-  # width = 6, height = 5*2, units = "in"
-  # width = 6, height = 18*2, units = "in"
-  width = 6, height = 8, units = "in"
+  plot = apall,
+  filename = file.path(getwd(), "figs", "2020_vs_2021_MAIN_supp.pdf"), 
+  width = 18, height = 24, units = "cm"
 )
 
 # main text figure --------------------------------------------------------
@@ -233,9 +264,8 @@ ggplot(
     axis.title.y = element_blank(), 
     panel.grid.major.x = element_blank(),
     legend.position = "bottom"
-  ) #+
-  # guides(color = guide_legend(nrow = 2, byrow = TRUE)) #+
-  # guides(linetype = guide_legend(nrow = 2))
+  )  +
+  xlab("Day of the Year")
 
 ggsave(
   filename = file.path(getwd(), "figs", "2020_vs_2021_MAIN.pdf"), 
