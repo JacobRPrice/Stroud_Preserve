24381f52d46e9f00cde5c61fda69460f69b15e69
JacobRPrice <JacobRPricePhD@gmail.com>
2023-01-09 13:26:14 -0500

    eea.R: moving to supplemental, plots altered.

--- src/eea.R
+++ src/eea.R
@@ -14,12 +14,13 @@ dat <- readRDS(file.path(getwd(), "/data/", "dat.RDS"))
   aes(
     x = `ln(BG)`,
     y = `ln(NAG)`, 
-    color = Treatment_Group
+    color = Treatment_Group, 
+    linetype = Year
   )
 ) +
   theme_bw() +
   geom_abline(slope = 1, intercept = 0) +
-  geom_point() +
+  geom_point(alpha = 0.50, size = 0.5) +
   geom_smooth(method = "lm", se = FALSE, size = 0.5)
 )
 
@@ -28,12 +29,13 @@ dat <- readRDS(file.path(getwd(), "/data/", "dat.RDS"))
   aes(
     x = `ln(AP)`,
     y = `ln(NAG)`, 
-    color = Treatment_Group
+    color = Treatment_Group, 
+    linetype = Year
   )
 ) +
     theme_bw() +
     geom_abline(slope = 1, intercept = 0) +
-    geom_point() +
+    geom_point(alpha = 0.50, size = 0.5) +
     geom_smooth(method = "lm", se = FALSE, size = 0.5)
 )
 
@@ -44,9 +46,9 @@ pNAGBG + pNAGAP +
     legend.position = "bottom", 
     legend.title = element_blank()
   ) &
-  guides(color = guide_legend(nrow = 3, byrow = FALSE))
+  guides(color = guide_legend(nrow = 5, byrow = FALSE))
 
 ggsave(
   filename = file.path(getwd(), "figs", "EEA_Ratio_Plots.pdf"),
-  width = 8.5, height = 2*8.5+2, units = "cm"
+  width = 10, height = 2*8.5+2, units = "cm"
 )
