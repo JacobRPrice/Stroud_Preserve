4893434a3704e855e0360b81c5c3c17fe4122b08
JacobRPrice <JacobRPricePhD@gmail.com>
2023-10-06 15:59:42 -0400

    seasonality.R: - moved each facet's axis labels from the top ribbon to the left axis. - only one smoothed line for each year - changed the height of both figures to 22 cm - change the main text filename to Figure_3.pdf

--- src/seasonality.R
+++ src/seasonality.R
@@ -69,14 +69,35 @@ datmain <- datmain %>% drop_na(value)
   theme_bw() +
   geom_point() +
   geom_smooth(se = FALSE, size = 0.5) +
-  facet_wrap(Parameter~., scales = "free_y", ncol = 2, dir = "v") +
+  facet_wrap(
+    Parameter~., scales = "free_y", ncol = 2, dir = "v", 
+    labeller = as_labeller(
+      c(
+        Net_Nitrification = "Net Nitrification",
+        Soil_NH4N = "Soil NH4-N", 
+        Net_Mineralization = "Net Mineralization",
+        Soil_NO3N = "Soil NO3-N", 
+        "log(AOA)" = "log(AOA)",
+        "log(AOB)" = "log(AOB)",
+        "log(nosZ)" = "log(nosZ)",
+        "ln(BG)" = "ln(BG)", 
+        "ln(NAG)" = "ln(NAG)",
+        "ln(AP)" = "ln(AP)",
+        "NAG:BG" = "NAG:BG", 
+        "NAG:AP" = "NAG:AP"
+      )
+    ),
+    strip.position = "left"
+  ) +
   scale_x_continuous(limits = c(60,340)) + 
   theme(
     axis.title.y = element_blank(), 
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank(),
-    legend.position = "bottom"
+    legend.position = "bottom", 
+    strip.background = element_blank(),
+    strip.placement = "outside"
   ) +
   guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
     guides(linetype = guide_legend(nrow = 2)) +
@@ -85,8 +106,8 @@ datmain <- datmain %>% drop_na(value)
 
 # ggsave(
 #   plot = apall,
-#   filename = file.path(getwd(), "figs", "2020_vs_2021_supp.pdf"), 
-#   width = 18, height = 24, units = "cm"
+#   filename = file.path(getwd(), "figs", "2020_vs_2021_supp.pdf"),
+#   width = 18, height = 22, units = "cm"
 # )
 
 # main text figure --------------------------------------------------------
@@ -95,25 +116,47 @@ ggplot(
   aes(
     x = Day,
     y = value, 
-    group = interaction(Year, Treatment_Group),
+    # group = interaction(Year, Treatment_Group),
     color = Year
   )
 ) +
   theme_bw() +
-  geom_point() +
+  geom_point(alpha = 0.5) +
   geom_smooth(se = FALSE, size = 0.5) +
-  facet_wrap(Parameter~., scales = "free_y", ncol = 2, dir = "v") +
+  facet_wrap(
+    Parameter~., scales = "free_y", ncol = 2, dir = "v", 
+    labeller = as_labeller(
+      c(
+        Net_Nitrification = "Net Nitrification",
+        Soil_NH4N = "Soil NH4-N", 
+        Net_Mineralization = "Net Mineralization",
+        Soil_NO3N = "Soil NO3-N", 
+        "log(AOA)" = "log(AOA)",
+        "log(AOB)" = "log(AOB)",
+        "log(nosZ)" = "log(nosZ)",
+        "ln(BG)" = "ln(BG)", 
+        "ln(NAG)" = "ln(NAG)",
+        "ln(AP)" = "ln(AP)",
+        "NAG:BG" = "NAG:BG", 
+        "NAG:AP" = "NAG:AP"
+      )
+    ),
+    strip.position = "left"
+  ) +
   scale_x_continuous(limits = c(60,340)) + 
   theme(
     axis.title.y = element_blank(), 
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank(),
-    legend.position = "bottom"
+    legend.position = "bottom", 
+    strip.background = element_blank(),
+    strip.placement = "outside"
   )  +
   xlab("Day of the Year")
 
 # ggsave(
-#   filename = file.path(getwd(), "figs", "2020_vs_2021_MAIN.pdf"), 
-#   width = 18, height = 24, units = "cm"
+##   filename = file.path(getwd(), "figs", "2020_vs_2021_MAIN.pdf"),
+#   filename = file.path(getwd(), "figs", "Figure_3.pdf"),
+#   width = 18, height = 22, units = "cm"
 # )
