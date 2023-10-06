b855cd5619444e2ae2cbfcdec16de28605fb38f1
JacobRPrice <JacobRPricePhD@gmail.com>
2023-10-06 15:53:21 -0400

    boxplots.R: - moved labels of individual facets from top ribbons to the sides - reduced x axis text to fit the new width from above - reduced the height of the figure to 22 cm - renamed filename to Figure_4.pdf

--- src/boxplots.R
+++ src/boxplots.R
@@ -59,17 +59,58 @@ ggplot(
   theme_bw() +
   geom_point(position = position_jitterdodge(jitter.height = 0), alpha = 0.5) +
   geom_boxplot() +
-  facet_wrap(Parameter~., scales = "free_y", ncol = 2, dir = "v") +
+  facet_wrap(
+    Parameter~., scales = "free_y", ncol = 2, dir = "v", 
+    labeller = as_labeller(
+      c(
+        # Net_Nitrification = bquote("Net Nit [mg N kg^-1 d^-1]")
+        # Net_Nitrification = expression(paste("Net Nit mg N", kg^-1))
+        # Net_Nitrification = Net~Nit~mg~N~kg^1
+        # Net_Nitrification = "Net Nit mg N (kg^(-1))"
+        # Net_Nitrification = expression("Net Nit mg N " ~ hr^-1)
+        
+        # Net_Nitrification = "Net Nitrification [mg N kg^-1 d^-1]",
+        # Soil_NH4N = "Soil NH4-N [mg N kg^-1]", 
+        # Net_Mineralization = "Net Mineralization [mg N kg^-1 d^-1]",
+        # Soil_NO3N = "Soil NO3-N [mg N kg^-1]", 
+        # "log(AOA)" = "AOA [Log10(copy number g^-1)]",
+        # "log(AOB)" = "AOB [Log10(copy number g^-1)]",
+        # "log(nosZ)" = "nosZ [Log10(copy number g^-1)]",
+        # "ln(BG)" = "BG [ln(µmol gOM^-1 hr^-1)]",
+        # "ln(NAG)" = "NAG [ln(µmol gOM^-1 hr^-1)]",
+        # "ln(AP)" = "AP [ln(µmol gOM^-1 hr^-1)]",
+        # "NAG:BG" = "NAG:BG", 
+        # "NAG:AP" = "NAG:AP"
+        
+        Net_Nitrification = "Net Nitrification",
+        Soil_NH4N = "Soil NH4-N", 
+        Net_Mineralization = "Net Mineralization",
+        Soil_NO3N = "Soil NO3-N", 
+        "log(AOA)" = "log(AOA)",
+        "log(AOB)" = "log(AOB)",
+        "log(nosZ)" = "log(nosZ)",
+        "ln(BG)" = "ln(BG)", 
+        "ln(NAG)" = "ln(NAG)",
+        "ln(AP)" = "ln(AP)",
+        "NAG:BG" = "NAG:BG", 
+        "NAG:AP" = "NAG:AP"
+      )
+    ),
+    strip.position = "left"
+  ) +
   theme(
     legend.position = "bottom", 
     axis.title.y = element_blank(), 
     panel.grid.major.x = element_blank(),
     panel.grid.minor.y = element_blank(),
-    axis.text.x = element_text(size = 8)
+    axis.text.x = element_text(size = 7), 
+    strip.background = element_blank(),
+    strip.placement = "outside"
   ) +
   xlab("Treatment Group")
 
 # ggsave(
-#   filename = file.path(getwd(), "figs", "boxplot_MAIN.pdf"), 
-#   width = 18, height = 24, units = "cm"
+# #   filename = file.path(getwd(), "figs", "boxplot_MAIN.pdf"),
+#   filename = file.path(getwd(), "figs", "Figure_4.pdf"),
+#   width = 18, height = 22, units = "cm"
 # )
